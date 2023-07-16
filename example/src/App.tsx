f3f3d94d51592a43ddb981fa0c6668bdbacf81e1
Ovikx <ovikdas10@gmail.com>
2023-07-16 02:53:32 -0400

    feat: delete functionality in example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
 
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 import { TodoList } from './components/TodoList';
 import { TodoInput } from './components/TodoInput';
 import { useEffect, useState } from 'react';
 import { todoTable } from './db/db';
 import type { Todo } from './types/types';
+import { RefreshContext } from './contexts';
 
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [numCompleted, setNumCompleted] = useState(0);
   const refreshTodos = () => {
     todoTable.select({}).then((res) => setTodos(res));
+    todoTable
+      .sum('completed', { completed: true })
+      .then((res) => setNumCompleted(res));
   };
   useEffect(refreshTodos, []);
 
   return (
-    <SafeAreaView className="flex-1 items-center bg-gray-900">
-      <TodoInput onInsert={refreshTodos} />
-      <TodoList todos={todos} />
-    </SafeAreaView>
+    <RefreshContext.Provider value={refreshTodos}>
+      <SafeAreaView className="flex-1 items-center bg-gray-900">
+        <Text className="text-white text-2xl font-bold">{`${numCompleted} tasks completed`}</Text>
+        <TodoInput onInsert={refreshTodos} />
+        <TodoList todos={todos} />
+      </SafeAreaView>
+    </RefreshContext.Provider>
   );
 }
--- example/src/components/TodoItem.tsx
+++ example/src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import type { Todo } from '../types/types';
 import { View, Text, Pressable } from 'react-native';
 import { todoTable } from '../db/db';
+import { RefreshContext } from '../contexts';
 
 interface Props {
   todo: Todo;
@@ -9,19 +10,35 @@ interface Props {
 
 export function TodoItem({ todo }: Props) {
   const [completed, setCompleted] = useState(todo.completed);
+  const refresh = useContext(RefreshContext);
   const onCheckboxPress = () => {
     todoTable
       .update({ completed: !completed }, { where: { title: todo.title } })
-      .then(() => setCompleted(!completed));
+      .then(() => {
+        setCompleted(!completed);
+        if (refresh) refresh();
+      });
+  };
+
+  const onDeletePress = () => {
+    todoTable
+      .delete({ where: { title: todo.title } })
+      .then(() => {
+        if (refresh) refresh();
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <View className="flex flex-row">
       <Pressable hitSlop={5} onPress={onCheckboxPress}>
-        <Text className="mx-2 text-white">{completed ? '[X]' : '[ ]'}</Text>
+        <Text className="mx-2 text-green-500">{completed ? '[X]' : '[ ]'}</Text>
       </Pressable>
       <Text className="mx-2 text-white">{todo.title}</Text>
       <Text className="mx-2 text-white">{todo.description}</Text>
+      <Pressable hitSlop={5} onPress={onDeletePress}>
+        <Text className="mx-2 text-red-500">DELETE</Text>
+      </Pressable>
     </View>
   );
 }
